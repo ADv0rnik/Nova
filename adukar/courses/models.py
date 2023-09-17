9d223bcfa7a83256f596b20f4e6e9009c2f44501
ADv0rnik <aadvornik@gmail.com>
2023-09-18 01:17:09 +0300

    Added scripts for code analysis

--- adukar/courses/models.py
+++ adukar/courses/models.py
@@ -4,64 +4,32 @@ from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 
-CAT_CHOICE = [
-    ("life", "life"),
-    ("computer", "computer")
-]
+CAT_CHOICE = [("life", "life"), ("computer", "computer")]
 
-EXERCISE_TYPE = [
-    ("watch", "watch"),
-    ("read", "read"),
-    ("quiz", "quiz")
-]
+EXERCISE_TYPE = [("watch", "watch"), ("read", "read"), ("quiz", "quiz")]
 
 
 class Course(models.Model):
-    title = models.CharField(
-        _('Title'),
-        max_length=250
-    )
-    num_lessons = models.IntegerField(
-        _('Number Of Lessons'),
-        default=0
-    )
+    title = models.CharField(_("Title"), max_length=250)
+    num_lessons = models.IntegerField(_("Number Of Lessons"), default=0)
     description = models.TextField(
-        _('Description'),
-        max_length=250,
-        null=True,
-        blank=True
+        _("Description"), max_length=250, null=True, blank=True
     )
-    category = models.CharField(
-        _('Category'),
-        choices=CAT_CHOICE,
-        max_length=100
-    )
+    category = models.CharField(_("Category"), choices=CAT_CHOICE, max_length=100)
     ranking = models.DecimalField(
-        _('Ranking'),
-        max_digits=3,
-        decimal_places=2,
-        default=0
+        _("Ranking"), max_digits=3, decimal_places=2, default=0
     )
     slug = models.SlugField(
-        max_length=255,
-        unique=True,
-        db_index=True,
-        verbose_name="URL"
+        max_length=255, unique=True, db_index=True, verbose_name="URL"
     )
-    created_at = models.DateTimeField(
-        _('Created at'),
-        auto_now_add=True
-    )
-    updated_at = models.DateTimeField(
-        _('Updated at'),
-        auto_now=True
-    )
+    created_at = models.DateTimeField(_("Created at"), auto_now_add=True)
+    updated_at = models.DateTimeField(_("Updated at"), auto_now=True)
 
     def __str__(self):
         return f"{self.pk} - {self.title}"
 
     def get_absolute_url(self):
-        return reverse('course', kwargs={'course_slug': self.slug})
+        return reverse("course", kwargs={"course_slug": self.slug})
 
     def get_module(self):
         return self.module_set.all()
@@ -71,49 +39,25 @@ class Course(models.Model):
 
 
 class Module(models.Model):
-    course = models.ForeignKey(
-        Course,
-        on_delete=models.CASCADE
-    )
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
     title = models.CharField(
-        _('Title'),
+        _("Title"),
         max_length=255,
     )
-    nm_exercises = models.IntegerField(
-        _('Number Of Exercises'),
-        default=0
-    )
-    image = models.ImageField(
-        _('Image'),
-        default='default.png',
-        null=True,
-        blank=True
-    )
-    overview = models.TextField(
-        _('Overview'),
-        blank=True,
-        null=True
-    )
+    nm_exercises = models.IntegerField(_("Number Of Exercises"), default=0)
+    image = models.ImageField(_("Image"), default="default.png", null=True, blank=True)
+    overview = models.TextField(_("Overview"), blank=True, null=True)
     slug = models.SlugField(
-        max_length=255,
-        unique=True,
-        db_index=True,
-        verbose_name="URL"
+        max_length=255, unique=True, db_index=True, verbose_name="URL"
     )
-    created_at = models.DateTimeField(
-        _('Created at'),
-        auto_now_add=True
-    )
-    updated_at = models.DateTimeField(
-        _('Updated at'),
-        auto_now=True
-    )
+    created_at = models.DateTimeField(_("Created at"), auto_now_add=True)
+    updated_at = models.DateTimeField(_("Updated at"), auto_now=True)
 
     def __str__(self):
         return f"{self.pk} {self.title}"
 
     def get_absolute_url(self):
-        return reverse('module', kwargs={'module_slug': self.slug})
+        return reverse("module", kwargs={"module_slug": self.slug})
 
     def get_exercise(self):
         return self.exercise_set.all()
@@ -123,48 +67,27 @@ class Module(models.Model):
 
 
 class Exercise(models.Model):
-    module = models.ForeignKey(
-        Module,
-        on_delete=models.CASCADE
-    )
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
     title = models.CharField(
-        _('Title'),
+        _("Title"),
         max_length=255,
     )
     description = models.TextField(
-        _('Description'),
-        max_length=250,
-        null=True,
-        blank=True
+        _("Description"), max_length=250, null=True, blank=True
     )
-    ex_type = models.CharField(
-        _('Type Of Exercise'),
-        choices=EXERCISE_TYPE
-    )
-    points = models.IntegerField(
-        _("Points"),
-        default=0
-    )
+    ex_type = models.CharField(_("Type Of Exercise"), choices=EXERCISE_TYPE)
+    points = models.IntegerField(_("Points"), default=0)
     slug = models.SlugField(
-        max_length=255,
-        unique=True,
-        db_index=True,
-        verbose_name="URL"
+        max_length=255, unique=True, db_index=True, verbose_name="URL"
     )
-    created_at = models.DateTimeField(
-        _('Created at'),
-        auto_now_add=True
-    )
-    updated_at = models.DateTimeField(
-        _('Updated at'),
-        auto_now=True
-    )
+    created_at = models.DateTimeField(_("Created at"), auto_now_add=True)
+    updated_at = models.DateTimeField(_("Updated at"), auto_now=True)
 
     def __str__(self):
         return f"{self.pk} {self.title}"
 
     def get_absolute_url(self):
-        return reverse('exercise', kwargs={'exercise_slug': self.slug})
+        return reverse("exercise", kwargs={"exercise_slug": self.slug})
 
     class Meta:
         verbose_name_plural = "Exercises"
