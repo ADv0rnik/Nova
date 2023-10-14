12b17c022b9c2da2004a4405a920412560362f44
ADv0rnik <aadvornik@gmail.com>
2023-10-15 01:26:27 +0300

    (WIP) #8 - create signin/signout page

--- adukar/profiles/views.py
+++ adukar/profiles/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from decorators import unauthenticated_user
+
+
+@unauthenticated_user
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username or password do not match')
+    return render(request, 'login.html')
