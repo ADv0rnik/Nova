b87ea09db5e497b54bc1d86448636fc66f66c083
ADv0rnik <aadvornik@gmail.com>
2023-12-01 22:27:33 +0300

    Created tests for Course and Category models

--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,50 @@
-from django.test import TestCase
+import pytest
+from django.test import TestCase, Client
 
-# Create your tests here.
+from courses.models import Category, Course
+
+
+@pytest.fixture(autouse=True)
+def use_dummy_cache_backend(settings):
+    settings.DEBUG = True
+
+
+class CategoryTestCase(TestCase):
+
+    @pytest.mark.django_db
+    def setUp(self) -> None:
+        self.client = Client()
+        self.category = Category.objects.create(name="Biology", slug="biology")
+
+    def test_category_name(self):
+        return self.assertEqual(self.category.name, "Biology")
+
+    def test_category_slug(self):
+        return self.assertEqual(self.category.slug, "biology")
+
+
+class CourseTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.category = Category.objects.create(name="Biology", slug="biology")
+        self.course = Course.objects.create(
+            title="Cell biology",
+            num_lessons=10,
+            category=self.category
+        )
+
+    def test_course_title(self):
+        return self.assertEqual(self.course.title, "Cell biology")
+
+    def test_course_category(self):
+        return self.assertEqual(self.course.category.name, self.category.name)
+
+    def test_str_call(self):
+        return self.assertEqual(self.course.__str__(), f"{self.course.pk} - {self.course.title}")
+
+    def test_request(self):
+        resp = self.client.get("/courses")
+        return self.assertEqual(resp.status_code, 301)
+
+
